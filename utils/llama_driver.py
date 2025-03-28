bd23fa2f645d39b23b8bca31b03b4ed54c76f817
nothern05 <gleb5shimko@gmail.com>
2025-03-28 11:09:20 +0200

    update prompts

--- utils/llama_driver.py
+++ utils/llama_driver.py
@@ -28,10 +28,10 @@ class LlamaDriver:
     def get_bullish_fud(self, symbol: str, post_data: dict, prompt: str, twitt_type: str):
         message = f"""Parse X posts with ${symbol.upper()}.
                 Posts data: {post_data}
-                Return 1 TOP {twitt_type.upper()} post data about {symbol.upper()} 'twitter_id' and 'twitter_user_id' of this post in JSON format.
+                Return TOP 1 {twitt_type.upper()} post data about {symbol.upper()} 'twitter_id' and 'twitter_user_id' of this post in JSON format.
                 (use engagement metrics to pick up the post from the {twitt_type.upper()} ones):""" + \
                 """###Output format
-                <{"twitter_user_id": <twitter_user_id>, "twitter_id": <twitter_id>}>
+                {"twitter_user_id": <twitter_user_id>, "twitter_id": <twitter_id>}
                 ###########################################"""
 
         data = {"prompt": prompt, "msg": message}
