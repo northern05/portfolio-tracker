aabcd18b25341c7570f7e1fc09e4d1ece6df3b93
nothern05 <gleb5shimko@gmail.com>
2025-04-04 14:12:50 +0300

    prompts update

--- utils/perplexity_driver.py
+++ utils/perplexity_driver.py
@@ -22,7 +22,7 @@ class PerplexityDriver:
         response = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
-            web_search_options={"search_context_size": "low"}
+            web_search_options={"search_context_size": "high"}
         )
         return response.choices[0].message.content
 
