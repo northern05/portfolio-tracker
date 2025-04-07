48248b0f2eecd4a562af19f8f80c2ae0aee80237
nothern05 <gleb5shimko@gmail.com>
2025-04-07 20:56:00 +0300

    update bot

--- app/api/portfolio/dependencies.py
+++ app/api/portfolio/dependencies.py
@@ -119,21 +119,21 @@ async def create_report(
         twitter: str = None
 ):
     for k, v in prompts.prompts.items():
-        perplexity_check = perplexity_driver.chat_without_streaming(
-            message=f"Answer only one word if any news about{full_token_name}",
-            prompt=v.get("check") % symbol
+        # perplexity_check = perplexity_driver.chat_without_streaming(
+        #     message=f"Answer only one word if any news about{full_token_name}",
+        #     prompt=v.get("check") % symbol
+        # )
+        # if perplexity_check.lower() == "no":
+        #     setattr(data, k, "No updates")
+        # else:
+        perplexity_result = perplexity_driver.chat_without_streaming(
+            message=v.get("msg") % (
+                symbol, full_token_name, twitter, datetime.now() - timedelta(days=7), datetime.now()),
+            prompt=v.get("perplexity_prompt") % (symbol, datetime.now() - timedelta(days=7), datetime.now())
         )
-        if perplexity_check.lower() == "no":
-            setattr(data, k, "No updates")
-        else:
-            perplexity_result = perplexity_driver.chat_without_streaming(
-                message=v.get("msg") % (
-                    symbol, full_token_name, twitter, datetime.now() - timedelta(days=7), datetime.now()),
-                prompt=v.get("perplexity_prompt") % (symbol, datetime.now() - timedelta(days=7), datetime.now())
-            )
-            llama_processing = llama.send_message(message=perplexity_result.removesuffix("</s>"),
-                                                  prompt=v.get("chatgpt_prompt"))
-            setattr(data, k, llama_processing)
+        llama_processing = llama.send_message(message=perplexity_result.removesuffix("</s>"),
+                                              prompt=v.get("chatgpt_prompt"))
+        setattr(data, k, llama_processing)
     twitts_over_asset = await twitter_scraper.fetch_tweets(protocol_name=twitter.split("/")[-1])
     if twitts_over_asset:
         msg = f"There is data from official {twitter} over {full_token_name} ${symbol} {twitts_over_asset}"
--- bot/bot.py
+++ bot/bot.py
@@ -279,7 +279,7 @@ async def get_report(callback: types.CallbackQuery):
         if price_movements:
             # formated_urls = format_urls_in_report(price_movements)
             escaped_report = escape_markdown(price_movements).removesuffix("</s>")
-            await callback.message.answer(f"📰{escaped_report}", parse_mode='MarkdownV2',
+            await callback.message.answer(f"📰 *Price movements: {escaped_report}*", parse_mode='MarkdownV2',
                                           disable_web_page_preview=True)
 
     response = requests.get(f"{API_URL}/selected/sentiment", params={"symbol": coin})
