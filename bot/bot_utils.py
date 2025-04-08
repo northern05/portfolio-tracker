34be06851d84dd5ae8e25c8722c90a1543131e80
nothern05 <gleb5shimko@gmail.com>
2025-04-08 12:37:09 +0300

    update bot

--- bot/bot_utils.py
+++ bot/bot_utils.py
@@ -60,7 +60,7 @@ def escape_markdown(text):
     """
     try:
         escape_chars = r'_*[]()~`>#+-=|{}.!'
-        result = re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text),
+        result = re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
     except Exception as e:
         return None
     return result
