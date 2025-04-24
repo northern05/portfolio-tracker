0b17389b8e1dbe63f9fc3ea71fc4497d2f4313a2
nothern05 <gleb5shimko@gmail.com>
2025-04-24 11:35:20 +0300

    UPDATE config

--- app/core/config.py
+++ app/core/config.py
@@ -51,7 +51,7 @@ class TwitterCredentialsSettings(BaseSettings):
 
 
 class LLamaSettings(BaseSettings):
-    LLAMA_URL: str = os.environ.get("LLAMA_URL", "http://195.189.60.154:8000/generate")
+    LLAMA_URL: str = os.environ.get("LLAMA_URL", "http://195.189.60.154:1111/v1/chat/completions")
 
 
 config = Config()
