f423730469b42b0f1afcda97f6affee4292d365e
nothern05 <gleb5shimko@gmail.com>
2025-04-25 15:48:52 +0300

    update bot

--- bot/bot_utils.py
+++ bot/bot_utils.py
@@ -58,7 +58,7 @@ def escape_markdown(text):
     """
     Escapes special characters for Telegram MarkdownV2 formatting.
     """
-    text = text.replace("**", "")
+    text = text.replace("**", "").replace("*", "")
     try:
         escape_chars = r'_*[]()~`>#+-=|{}.!'
         result = re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text).replace("</s>", "")
