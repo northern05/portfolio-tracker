b628b6e2f73bbc8ebfb366144a4adef66ee41363
nothern05 <gleb5shimko@gmail.com>
2025-04-25 09:30:53 +0300

    update parsing

--- app/api/portfolio/dependencies.py
+++ app/api/portfolio/dependencies.py
@@ -115,12 +115,18 @@ async def get_sentiment_score(
         prompt=prompts.top_1_bullish % portfolio.symbol,
         message=f"Choose TOP 1 Bullish twitter post about ${portfolio.symbol} ({full_token_name} project): {sorted_score_list[:5]}."
     )
-    bullish = await create_twitt_url(data=ast.literal_eval(bullish_post)[0])
+    extracted_json = extract_json(bullish_post)
+    bullish = None
+    if isinstance(extracted_json, dict):
+        bullish = await create_twitt_url(data=extracted_json)
     fud_post = llama.send_message(
         prompt=prompts.top_1_fud % portfolio.symbol,
         message=f"Choose TOP 1 Bearish/FUD twitter post about ${portfolio.symbol} ({full_token_name} project): {sorted_score_list[-5:]}."
     )
-    fud = await create_twitt_url(data=ast.literal_eval(fud_post)[0])
+    fud = None
+    extracted_json = extract_json(fud_post)
+    if isinstance(extracted_json, dict):
+        fud = await create_twitt_url(data=extracted_json)
     response_data = SentimentScore(bullish=bullish, fud=fud)
     await redis_db.set(f"{portfolio.symbol}_sentiment", response_data.json(), ex=86400)
     return response_data
@@ -134,6 +140,19 @@ def extract_rating(text):
         return match.group(1)  # Return the captured digits
     else:
         return 0
+
+def extract_json(data: str):
+    if data.startswith('[') and data.endswith(']'):
+        try:
+            parsed = ast.literal_eval(data)
+            post = await create_twitt_url(data=parsed[0])
+        except (SyntaxError, ValueError) as e:
+            print(f"Failed to parse fud_post: {e}")
+            post = None
+    else:
+        print("Invalid input format:", data)
+        post = None
+    return post
 
 
 async def create_twitt_url(data: dict):
